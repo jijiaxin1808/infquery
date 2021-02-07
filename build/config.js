01422a7599d7489e8dc8159d0554fbbb736b158e
潘神1808 <44405174+jijiaxin1808@users.noreply.github.com>
2021-02-07 14:57:27 +0800

    build: (rollup)优化打包

--- build/config.js
+++ build/config.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const { moduleName, dependencies } = require('../package.json');
 const { babel } = require('@rollup/plugin-babel');
@@ -5,57 +7,56 @@ const nodeResolve = require('@rollup/plugin-node-resolve').default;
 const commonjs = require('@rollup/plugin-commonjs');
 const replace = require('@rollup/plugin-replace');
 const alias = require('@rollup/plugin-alias');
-const { terser } = require('rollup-plugin-terser');
-const externalGlobals = require('rollup-plugin-external-globals');
 const extensions = ['.js', '.ts'];
 const DEV = 'development';
 
 const resolveFile = function (filePath) {
-  return path.join(__dirname, '..', filePath);
+    return path.join(__dirname, '..', filePath);
 };
 
 const builds = {
     output: {
-      file: resolveFile('dist/index.cjs.js'),
-      format: 'cjs',
+        file: resolveFile('dist/index.cjs.js'),
+        format: 'cjs',
     },
     external: Object.keys(dependencies),
 };
 
 function genConfig(name) {
-  const opts = builds
-  const config = {
-    ...opts,
-    cache: true,
-    input: resolveFile('src/index.ts'),
-    output: {
-      name: moduleName,
-      ...opts.output,
-      sourcemap: opts.env === DEV,
-    },
-    plugins: [
-      nodeResolve({ extensions }),
-      commonjs(),
-      babel({
-        extensions,
-        exclude: 'node_modules/**',
-        babelHelpers: 'runtime',
-      }),
-      alias({
-        resolve: extensions,
-        entries: [
-          {
-            find: '@',
-            replacement: path.resolve(__dirname, '../src'),
-          },
-        ],
-      }),
-      replace({
-        'process.env.NODE_ENV': JSON.stringify(opts.env),
-      }),
-    ].concat(opts.plugins || []),
-  };
-  return config;
+    const opts = builds;
+    const config = {
+        ...opts,
+        cache: true,
+        input: resolveFile('src/index.ts'),
+        output: {
+            name: moduleName,
+            ...opts.output,
+            sourcemap: opts.env === DEV,
+        },
+        plugins: [
+            nodeResolve({ extensions }),
+            commonjs(),
+            babel({
+                extensions,
+                exclude: 'node_modules/**',
+                babelHelpers: 'runtime',
+                configFile: path.resolve(__dirname, './.babelrc.json')
+            }),
+            alias({
+                resolve: extensions,
+                entries: [
+                    {
+                        find: '@',
+                        replacement: path.resolve(__dirname, '../src'),
+                    },
+                ],
+            }),
+            replace({
+                'process.env.NODE_ENV': JSON.stringify(opts.env),
+            }),
+        ].concat(opts.plugins || []),
+    };
+    return config;
 }
 
 module.exports = genConfig();
